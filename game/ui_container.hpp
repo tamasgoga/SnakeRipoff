bb05b6c740f5b247776f09b717174e92be900929
Tamas Goga <tamasgoga94@gmail.com>
2018-02-05 16:09:17 +0200

    making sure everything is committed

--- game/ui_container.hpp
+++ game/ui_container.hpp
@@ -6,15 +6,25 @@
 // Container widgets with scrolling overflow
 //--------------------------------------------------------------
 
+#include "ui_widget.hpp"
 #include <SDL2/SDL_rect.h>
+#include <vector>
 
 
-class Container {
+// A container should only exist until it's widgets are there
+class Container: public Widget {
 public:
+	using container_type = std::vector<const Widget*>;
+	using index = container_type::size_type;
+
 	Container(int x, int y, int w, int h) noexcept;
 	virtual ~Container();
 
-	void draw() {
+	index add(const Widget& w);
+
+	bool remove(index i);
+
+	void draw() const {
 		return;
 	}
 
@@ -23,7 +33,14 @@ public:
 	}
 
 private:
+	// Bounding box
 	SDL_Rect box;
+
+	// index of the next inserted elem
+	index nextIndex;
+
+	// Non-owning pointers
+	std::vector<const Widget*> container;
 };
 
 
